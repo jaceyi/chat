77db87e42d1a634cf9a88c34d84826c1a7c68e7f
jaceyi <yijinchun@outlook.com>
2022-01-06 11:52:30 +0800

    feat: update @user messaging

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,53 @@
 const functions = require('firebase-functions');
-const { initializeApp, messaging } = require('firebase-admin');
+const admin = require('firebase-admin');
 
-initializeApp();
+admin.initializeApp();
+
+const getBlocksText = (blocks, maxCount = 20) => {
+  let text = '';
+  for (const item of blocks) {
+    if (text.length > maxCount) {
+      return text.slice(0, maxCount) + '...';
+    }
+    text += item.text;
+  }
+  return text;
+};
 
 exports.sendMessage = functions.database
-  .ref('/messages')
+  .ref('/messages/{messageId}')
   .onCreate(async (snapshot, context) => {
+    const { messageId } = context.params;
     try {
-      const original = snapshot.val();
-      const snap = await database().ref('messageTokens').once('value');
-      let tokens = [];
-      if (snap && snap.exists()) {
-        tokens = Object.values(snap.val());
+      const tokenSnapshot = await admin
+        .database()
+        .ref('messageTokens')
+        .once('value');
+      if (!tokenSnapshot.exists()) return null;
+      const tokenMap = tokenSnapshot.val();
+      const message = snapshot.val();
+      const tokens = [];
+      if (!message.raw.entityMap) return null;
+      for (const entity of message.raw.entityMap) {
+        if (entity.type !== 'USER') continue;
+        // 只有包含有 USER(@) 的 type 才发消息
+        const token = tokenMap[entity.data.uid];
+        token && tokens.push(token);
       }
-      functions.logger.log('sendMessage', context.params.pushId, {
-        sendContent: original,
-        tokens
-      });
+
+      if (!tokens.length) return null;
       const payload = {
         notification: {
-          title: '有新消息！',
-          body: 'Test body'
+          title: '有人@你~',
+          body: getBlocksText(message.raw.blocks) || '...'
         }
       };
-      messaging().sendToDevice(tokens, payload);
+      admin.messaging().sendToDevice(tokens, payload);
+      return functions.logger.log('Send success', messageId, {
+        message,
+        tokens
+      });
     } catch (e) {
-      console.log(e);
+      return functions.logger.error('Send error', messageId, e);
     }
-    return null;
   });

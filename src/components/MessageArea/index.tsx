7c65deb187e28f8e4d8430c65994d193e5b0835f
jaceyi <yijinchun@outlook.com>
2022-12-28 15:23:15 +0800

    feat: add auto scrollToIndex

--- src/components/MessageArea/index.tsx
+++ src/components/MessageArea/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import { useEffect, useRef } from 'react';
 import type { FC } from 'react';
 import Message, { MessageInfo } from './Message';
 import * as styles from './style.module.scss';
 import Loading from './Loading';
-import { Virtuoso } from 'react-virtuoso';
+import { Virtuoso, VirtuosoHandle } from 'react-virtuoso';
 
 export type MessageList = MessageInfo[];
 
@@ -13,12 +14,22 @@ interface MessageAreaProps {
 }
 
 const MessageArea: FC<MessageAreaProps> = ({ messageList, loading }) => {
+  const virtuosoRef = useRef<VirtuosoHandle>(null);
+  useEffect(() => {
+    if (!virtuosoRef.current) return;
+    virtuosoRef.current.scrollToIndex({
+      index: messageList.length - 1,
+      behavior: 'smooth'
+    });
+  }, [messageList]);
+
   return (
     <div className={styles.container}>
       {loading ? (
         <Loading />
       ) : (
         <Virtuoso
+          ref={virtuosoRef}
           initialTopMostItemIndex={messageList.length - 1}
           data={messageList}
           overscan={500}
